a64662e0dc3b274fd99d588842a4cbceed1a54f6
Matthew Gomes <matthew.h.gomes@gmail.com>
2022-10-29 17:22:10 -0400

    Flowing fur compatibility (#1)
    * Start working on flowing fur compatibility- this commit will result in a bunch of duplicate furs.
    
    * Start code to cull multiple furs on the same NPC
    
    * Actually works now, still causes duplicate furs

--- SkyFurry_Visual_Patcher/Program.cs
+++ SkyFurry_Visual_Patcher/Program.cs
@@ -5,6 +5,7 @@ using Noggog;
 using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Plugins.Records;
 using System.Xml.Linq;
+using SkyFurry_Visual_Patcher.Utilities;
 
 namespace SkyFurry_Visual_Patcher {
     public class Program {
@@ -17,14 +18,13 @@ namespace SkyFurry_Visual_Patcher {
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state) {
             //check for SkyFurry
-
             ISkyrimModGetter? SkyFurry = state.LoadOrder[state.LoadOrder.Keys.Where(x => ((string)x.FileName).ToLower() == "SkyFurry.esp".ToLower()).ToList().First()].Mod;
 
             if (SkyFurry == null) {
                 System.Console.WriteLine("SkyFurry.esp not found");
                 return;
             }
-
+            System.Console.WriteLine("Patching base SkyFurry visuals\n");
             //get list of mods which inherit from SkyFurry, as these may contain NPCs we need to patch
             List<ISkyrimModGetter> modsToPatch = new();
             List<String> modNames = new();
@@ -92,23 +92,91 @@ namespace SkyFurry_Visual_Patcher {
 
                         //copy race
                         patchNpc.Race.SetTo(npc.Race);
-                        if (patchNpc.Equals(npc)) {
-                            state.PatchMod.Npcs.Remove(npc);
-                            ignored++;
-                        }
-                        processed++;
 
                         //copy weight
                         patchNpc.Weight = npc.Weight;
 
                         //copy height
                         patchNpc.Height = npc.Height;
+
+
+                        //remove unchanged NPCs
+                        if (patchNpc.Equals(winningOverride)) {
+                            state.PatchMod.Npcs.Remove(npc);
+                            ignored++;
+                        }
+
+                        processed++;
                     }
                 }
                 else {
                     System.Console.WriteLine("No NPCs found");
                 }
                 System.Console.WriteLine("Ignoring " + ignored + " unchanged NPCs");
+            }
+            //Now check for flowing fur addon and forward changes from it and anything that inherits from it
+            ISkyrimModGetter? FlowingFur = state.LoadOrder[state.LoadOrder.Keys.Where(x => ((string)x.FileName).ToLower() == "SkyFurry_FlowingFur.esp".ToLower()).ToList().First()].Mod;
+            System.Console.WriteLine("\nChecking for flowing fur...");
+            if (FlowingFur != null) {
+                System.Console.WriteLine("Found!");
+                //get a list of furs added by SkyFurry_FlowingFur.esp so that we can detect them in NPCs
+                List<String> furTypes = new();
+                foreach (IArmorGetter furType in FlowingFur.Armors) {
+                    if (furType.EditorID is not null) {
+                        furTypes.Add(furType.EditorID.ToString());
+                    }
+                }
+                (modNames, modsToPatch) = state.LoadOrder.getPatchableModsFromMaster("SkyFurry_FlowingFur.esp", FlowingFur);
+                ModIndex = -1;
+                foreach (ISkyrimModGetter mod in modsToPatch) {
+                    int processed = 0;
+                    int ignored = 0;
+                    int total = mod.Outfits.Count;
+                    var modFormIDs = mod.Outfits.Select(x => x.FormKey).ToList();
+                    var winningOverrides = state.LoadOrder.PriorityOrder.WinningOverrides<IOutfitGetter>().Where(x => modFormIDs.Contains(x.FormKey)).ToList();
+                    ModIndex++;
+                    System.Console.WriteLine("\nImporting furs from: " + modNames[ModIndex]);
+                    if (mod.Outfits.Count > 0) {
+                        foreach (IOutfitGetter outfit in mod.Outfits) {
+                            if (processed % 10 == 0) {
+                                System.Console.WriteLine(processed + "/" + total + " outfits");
+                            }
+                            IOutfitGetter winningOverride = winningOverrides.Where(x => x.FormKey == outfit.FormKey).First();
+                            Outfit patchOutfit = state.PatchMod.Outfits.GetOrAddAsOverride(winningOverride);
+                            if (outfit.Items is not null) {
+                                List<IOutfitTargetGetter> fursToRemove = new();
+                                foreach (IFormLinkGetter<IOutfitTargetGetter> itemForm in outfit.Items) {
+                                    IOutfitTargetGetter? item = itemForm.TryResolve(state.LinkCache);
+                                    if (item is not null && item.EditorID is not null && furTypes.Contains(item.EditorID)){
+                                        //Apparently the item list can be null, so if we need to add fur we need to check for this
+                                        if (patchOutfit.Items is null) {
+                                            patchOutfit.Items = new ExtendedList<IFormLinkGetter<IOutfitTargetGetter>>();
+                                        }
+                                        //mark any existing furs for deletion. We can't delete them here because we can't iterate on an ExtendedList and modify it at the same time
+                                        foreach (IFormLinkGetter<IOutfitTargetGetter> potentialFurForm in patchOutfit.Items) {
+                                            IOutfitTargetGetter? potentialFurItem = potentialFurForm.TryResolve(state.LinkCache);
+                                            if (potentialFurItem is not null && potentialFurItem.EditorID is not null && furTypes.Contains(potentialFurItem.EditorID)) {
+                                                fursToRemove.Add(potentialFurItem);
+                                            }
+                                        }
+                                        patchOutfit.Items.Add(itemForm);
+                                    }
+                                }
+                            }
+                            //remove unchanged outfits
+                            if (patchOutfit.Equals(winningOverride)) {
+                                state.PatchMod.Outfits.Remove(outfit);
+                                ignored++;
+                            }
+
+                            processed++;
+                        }
+                    }
+                    else {
+                        System.Console.WriteLine("No NPCs found");
+                    }
+                    System.Console.WriteLine("Ignoring " + ignored + " unchanged Outfits");
+                }
             }
         }
     }
