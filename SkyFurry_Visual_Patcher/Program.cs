4f4a9799de77e7f2cb54c91512897c7f8e99350e
Matthew Gomes <matthew.h.gomes@gmail.com>
2022-10-29 04:22:33 -0400

    Start working on flowing fur compatibility- this commit will result in a bunch of duplicate furs.

--- SkyFurry_Visual_Patcher/Program.cs
+++ SkyFurry_Visual_Patcher/Program.cs
@@ -5,6 +5,7 @@ using Noggog;
 using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Plugins.Records;
 using System.Xml.Linq;
+using SkyFurry_Visual_Patcher.Utilities;
 
 namespace SkyFurry_Visual_Patcher {
     public class Program {
@@ -17,14 +18,13 @@ namespace SkyFurry_Visual_Patcher {
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state) {
             //check for SkyFurry
-
             ISkyrimModGetter? SkyFurry = state.LoadOrder[state.LoadOrder.Keys.Where(x => ((string)x.FileName).ToLower() == "SkyFurry.esp".ToLower()).ToList().First()].Mod;
 
             if (SkyFurry == null) {
                 System.Console.WriteLine("SkyFurry.esp not found");
                 return;
             }
-
+            System.Console.WriteLine("Patching base SkyFurry visuals\n");
             //get list of mods which inherit from SkyFurry, as these may contain NPCs we need to patch
             List<ISkyrimModGetter> modsToPatch = new();
             List<String> modNames = new();
@@ -109,6 +109,54 @@ namespace SkyFurry_Visual_Patcher {
                     System.Console.WriteLine("No NPCs found");
                 }
                 System.Console.WriteLine("Ignoring " + ignored + " unchanged NPCs");
+            }
+            //Now check for flowing fur addon and forward changes from it and anything that inherits from it
+            ISkyrimModGetter? FlowingFur = state.LoadOrder[state.LoadOrder.Keys.Where(x => ((string)x.FileName).ToLower() == "SkyFurry_FlowingFur.esp".ToLower()).ToList().First()].Mod;
+            System.Console.WriteLine("\nChecking for flowing fur...");
+            if (FlowingFur != null) {
+                System.Console.WriteLine("Found!");
+                (modNames, modsToPatch) = state.LoadOrder.getPatchableModsFromMaster("SkyFurry_FlowingFur.esp", FlowingFur);
+                foreach (ISkyrimModGetter mod in modsToPatch) {
+                    int processed = 0;
+                    int ignored = 0;
+                    int total = mod.Npcs.Count;
+                    var modFormIDs = mod.Npcs.Select(x => x.FormKey).ToList();
+                    var winningOverrides = state.LoadOrder.PriorityOrder.WinningOverrides<INpcGetter>().Where(x => modFormIDs.Contains(x.FormKey)).ToList();
+                    ModIndex++;
+                    System.Console.WriteLine("\nImporting visuals from: " + modNames[ModIndex]);
+                    if (mod.Npcs.Count > 0) {
+                        foreach (INpcGetter npc in mod.Npcs) {
+                            if (processed % 10 == 0) {
+                                System.Console.WriteLine(processed + "/" + total + " Npcs");
+                            }
+                            var winningOverride = winningOverrides.Where(x => x.FormKey == npc.FormKey).First();
+                            var patchNpc = state.PatchMod.Npcs.GetOrAddAsOverride(winningOverride);
+                            if (npc.Items is not null) {
+                                foreach (IContainerEntryGetter item in npc.Items) {
+                                    //get a list of furs added by SkyFurry_FlowingFur.esp so that we can detect them in NPCs
+                                    List<String> furTypes = new();
+                                    foreach (IArmorGetter furType in FlowingFur.Armors) {
+                                        if (furType.EditorID is not null) {
+                                            furTypes.Add(furType.EditorID.ToString());
+                                        }
+                                    }
+                                    String? itemName = item.Item.ToString();
+                                    if (itemName is not null && furTypes.Contains(itemName)){
+                                        //Apparently the item list can be null, so if we need to add fur we need to check for this
+                                        if (patchNpc.Items is null) {
+                                            patchNpc.Items = new ExtendedList<ContainerEntry>();
+                                        }
+                                        patchNpc.Items.Add(item.DeepCopy());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        System.Console.WriteLine("No NPCs found");
+                    }
+                    System.Console.WriteLine("Ignoring " + ignored + " unchanged NPCs");
+                }
             }
         }
     }
---
+++ SkyFurry_Visual_Patcher/Utilities/LoadOrderUtilities.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+using System.Text;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Order;
+using Mutagen.Bethesda.Plugins.Records;
+
+namespace SkyFurry_Visual_Patcher.Utilities {
+    public static class LoadOrderUtilities {
+        public static (List<String>, List<ISkyrimModGetter>) getPatchableModsFromMaster(this ILoadOrder<IModListing<ISkyrimModGetter>> LoadOrder, String masterName, ISkyrimModGetter master) {
+            List<ISkyrimModGetter> modsToPatch = new();
+            List<String> modNames = new();
+            modsToPatch.Add(master);
+            modNames.Add(masterName);
+            foreach (ModKey modName in LoadOrder.Keys.ToList()) {
+                var mod = LoadOrder[modName].Mod;
+                if (mod is not null)
+                    foreach (IMasterReferenceGetter currentMaster in mod.MasterReferences) {
+                        //list mods that inherit from FlowingFur and are not synthesis.esp
+                        if (currentMaster.Master.ToString().Equals("SkyFurry.esp") && !(modName.ToString().ToLower().Equals("synthesis.esp") || modName.ToString().ToLower().Equals("requiem for the indifferent.esp"))) {
+                            modsToPatch.Add(mod);
+                            modNames.Add(modName.ToString());
+                        }
+                    }
+            }
+            return (modNames, modsToPatch);
+        }
+    }
+}
