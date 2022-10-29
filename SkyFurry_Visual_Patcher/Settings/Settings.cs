1f9d911ab72b223671921c3ab912ff0dc7e2a322
Matthew Gomes <matthew.h.gomes@gmail.com>
2022-10-29 18:27:06 -0400

    Aff user configuration settings, improve method of ignoring unwanted mods

---
+++ SkyFurry_Visual_Patcher/Settings/Settings.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyFurry_Visual_Patcher.Settings {
+    class Settings {
+        public bool ignoreIdenticalToWinningOverride { get; set; } = true;
+        public bool mergeVisuals { get; set; } = true;
+        public bool mergeFlowingFur { get; set; } = true;
+    }
+}
