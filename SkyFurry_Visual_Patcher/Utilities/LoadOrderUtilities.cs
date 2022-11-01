fdcbca9716376d24e83a2a371f2adf084edd0c9c
Matthew Gomes <matthew.h.gomes@gmail.com>
2022-11-01 18:16:42 -0400

    Patch race visuals (#4)
    * continued development, finished actor effects, skin, keywords, and body template
    
    * Finish race patching

--- SkyFurry_Visual_Patcher/Utilities/LoadOrderUtilities.cs
+++ SkyFurry_Visual_Patcher/Utilities/LoadOrderUtilities.cs
@@ -14,11 +14,27 @@ namespace SkyFurry_Visual_Patcher.Utilities {
         public static ISkyrimModGetter? getModByFileName(this ILoadOrder<IModListing<ISkyrimModGetter>> LoadOrder, string name) {
             return LoadOrder[LoadOrder.Keys.Where(x => ((string)x.FileName).ToLower() == name.ToLower()).ToList().First()].Mod;
         }
-        public static (List<String>, List<ISkyrimModGetter>) getModsFromMaster(this ILoadOrder<IModListing<ISkyrimModGetter>> LoadOrder, String masterName, ISkyrimModGetter master) {
+        public static (List<String>, List<ISkyrimModGetter>) getModsFromMasterIncludingMaster(this ILoadOrder<IModListing<ISkyrimModGetter>> LoadOrder, String masterName, ISkyrimModGetter master) {
             List<ISkyrimModGetter> modsToPatch = new();
             List<String> modNames = new();
             modsToPatch.Add(master);
             modNames.Add(masterName);
+            foreach (ModKey modName in LoadOrder.Keys.ToList()) {
+                var mod = LoadOrder[modName].Mod;
+                if (mod is not null)
+                    foreach (IMasterReferenceGetter currentMaster in mod.MasterReferences) {
+                        //list mods that inherit from master
+                        if (currentMaster.Master.ToString().Equals(masterName) && !(modName.ToString().ToLower().Equals("synthesis.esp") || modName.ToString().ToLower().Equals("requiem for the indifferent.esp"))) {
+                            modsToPatch.Add(mod);
+                            modNames.Add(modName.ToString());
+                        }
+                    }
+            }
+            return (modNames, modsToPatch);
+        }
+        public static (List<String>, List<ISkyrimModGetter>) getModsFromMaster(this ILoadOrder<IModListing<ISkyrimModGetter>> LoadOrder, String masterName, ISkyrimModGetter master) {
+            List<ISkyrimModGetter> modsToPatch = new();
+            List<String> modNames = new();
             foreach (ModKey modName in LoadOrder.Keys.ToList()) {
                 var mod = LoadOrder[modName].Mod;
                 if (mod is not null)
