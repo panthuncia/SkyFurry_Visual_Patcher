075bb5694f75fd88f7ad855ac2ca793002214bf4
Matthew Gomes <matthew.h.gomes@gmail.com>
2022-11-02 00:34:34 -0400

    Add support for digiboots

--- SkyFurry_Visual_Patcher/Settings/Settings.cs
+++ SkyFurry_Visual_Patcher/Settings/Settings.cs
@@ -7,13 +7,16 @@ using System.Threading.Tasks;
 namespace SkyFurry_Visual_Patcher.Settings {
     class Settings {
         public bool ignoreIdenticalToWinningOverride { get; set; } = true;
-        public bool mergeNpcVisuals { get; set; } = true;
+        public bool patchNpcVisuals { get; set; } = true;
         public bool mergeRaces { get; set; } = true;
         public bool mergeFlowingFur { get; set; } = true;
         public bool mergeSharpClaws { get; set; } = true;
+        public bool mergeDigiBoots { get; set; } = true;
         public bool scaleUnarmedDamageWithWinningOverride { get; set; } = true;
         public String SkyFurryBaseModName { get; set; } = "SkyFurry.esp";
         public String FlowingFurModName { get; set; } = "SkyFurry_FlowingFur.esp";
         public String SharpClawsModName { get; set; } = "SkyFurry_SharpClaws.esp";
+        public String DigibootsModName { get; set; } = "SF_Digiboots.esp";
+
     }
 }
